b6ea1f8ab0bacf3b86bdeb78e992513e2cf3b46f
cizeautom <cizeautom404@gmail.com>
2026-01-08 17:26:38 +0100

    projet fini d'un dico pour le langage des signes simplistes

---
+++ script.js
@@ -0,0 +1,12 @@
+function search(){
+    const research = document.getElementById("search").value;
+
+    const displayText = document.getElementById("searched");
+    displayText.textContent = research;
+
+    const img = document.getElementById("searched-img");
+
+    research.toLowerCase();
+
+    img.src = `alphabet/${research}.jpg`;
+}
